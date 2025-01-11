17bd4f311e82f54e21609db300ec309cacbc4b9b
irfanca1 <irfanchoiruddin23@gmail.com>
2025-01-11 12:18:18 +0700

    Update: intro.jsx & footer.jsx

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './Intro.css';
 import Github from '../../img/github.png';
 import LinkedIn from '../../img/linkedin.png';
@@ -9,67 +9,51 @@ import Boy from '../../img/boy.png';
 import Thumbup from '../../img/thumbup.png';
 import Glassesimoji from '../../img/glassesimoji.png';
 import Crown from '../../img/crown.png';
-import FloatingDiv from "../FloatingDiv/FloatingDiv";
-import {motion} from 'framer-motion';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+import { motion } from 'framer-motion';
 
 const Intro = () => {
-    const transition = { duration: 2, type: 'spring'}
-    return (
-        <div className="intro">
-            <div className="i-left">
-                <div className="i-name">
-                    <span>Hy! I Am</span>
-                    <span>Irfan Choiruddin</span>
-                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quam error quia consectetur laborum, mollitia blanditiis, qui, fuga cumque autem assumenda sint et voluptatum odit voluptates animi est ea quas.</span>
-                </div>
-                <button className="button i-button">Hire me</button>
-                <div className="i-icons">
-                    <a href="">
-                        <img src={Github} alt="Github" />
-                    </a>
-                    <a href="">
-                        <img src={LinkedIn} alt="LinkedIn" />
-                    </a>
-                    <a href="">
-                        <img src={Instagram} alt="Instagram" />
-                    </a>
-                </div>
-            </div>
-            <div className="i-right">
-                <img src={Vector1} alt="Vector1" />
-                <img src={Vector2} alt="Vector2" />
-                <img src={Boy} alt="boy" />
-                <motion.img 
-                initial={{left: '-36%'}}
-                whileInView={{left: '-24%'}}
-                transition={transition}
-                src={Glassesimoji} alt="Glassesimoji" 
-                className='floating-div'/>
-
-                <motion.div 
-                initial={{top: '-4%', left: '74%'}}
-                whileInView={{left: '68%'}}
-                transition={transition}
-                style={{top: '-4%', left: '68%'}}
-                className='floating-div'
-                >
-                    <FloatingDiv image={Crown} txt1='Web' txt2='Developer' />
-                </motion.div>
-
-                <motion.div 
-                initial={{left: '9rem', top: '18rem'}}
-                whileInView={{left: '0rem'}}
-                transition={transition}
-                style={{top: '18rem'}}
-                className='floating-div'>
-                    <FloatingDiv image={Thumbup} txt1='Best Desgin' txt2='Award' />
-                </motion.div>
-
-                <div className="blur" style={{background: 'rgb(238 210 255)'}}></div>
-                <div className="blur" style={{background: '#c1f5ff', top: '17rem', width: '21rem', height: '11rem', left: '-9rem'}}></div>
-            </div>
+  const transition = { duration: 2, type: 'spring' };
+  return (
+    <div className="intro">
+      <div className="i-left">
+        <div className="i-name">
+          <span>Hy! I Am</span>
+          <span>Irfan Choiruddin</span>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quam error quia consectetur laborum, mollitia blanditiis, qui, fuga cumque autem assumenda sint et voluptatum odit voluptates animi est ea quas.</span>
         </div>
-    )
-}
+        <button className="button i-button">Hire me</button>
+        <div className="i-icons">
+          <a href="https://github.com/Irfanca1" target="_blank">
+            <img src={Github} alt="Github" />
+          </a>
+          <a href="https://www.linkedin.com/in/irfan-choiruddin-anwar-559b94237/" target="_blank">
+            <img src={LinkedIn} alt="LinkedIn" />
+          </a>
+          <a href="https://www.instagram.com/cchhhooii" target="_blank">
+            <img src={Instagram} alt="Instagram" />
+          </a>
+        </div>
+      </div>
+      <div className="i-right">
+        <img src={Vector1} alt="Vector1" />
+        <img src={Vector2} alt="Vector2" />
+        <img src={Boy} alt="boy" />
+        <motion.img initial={{ left: '-36%' }} whileInView={{ left: '-24%' }} transition={transition} src={Glassesimoji} alt="Glassesimoji" className="floating-div" />
+
+        <motion.div initial={{ top: '-4%', left: '74%' }} whileInView={{ left: '68%' }} transition={transition} style={{ top: '-4%', left: '68%' }} className="floating-div">
+          <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
+        </motion.div>
+
+        <motion.div initial={{ left: '9rem', top: '18rem' }} whileInView={{ left: '0rem' }} transition={transition} style={{ top: '18rem' }} className="floating-div">
+          <FloatingDiv image={Thumbup} txt1="Best Desgin" txt2="Award" />
+        </motion.div>
+
+        <div className="blur" style={{ background: 'rgb(238 210 255)' }}></div>
+        <div className="blur" style={{ background: '#c1f5ff', top: '17rem', width: '21rem', height: '11rem', left: '-9rem' }}></div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
